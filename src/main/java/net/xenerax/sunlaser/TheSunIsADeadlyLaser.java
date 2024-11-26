86fedc4a40e93915e4af72e0b3b826bb91f1d6a3
xenerax <saeland.florian@gmail.com>
2024-11-26 23:17:36 +0100

    Initial commit

---
+++ src/main/java/net/xenerax/sunlaser/TheSunIsADeadlyLaser.java
@@ -0,0 +1,20 @@
+package net.xenerax.sunlaser;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheSunIsADeadlyLaser implements ModInitializer {
+	public static final String MOD_ID = "sunlaser";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/xenerax/sunlaser/TheSunIsADeadlyLaserClient.java
@@ -0,0 +1,10 @@
+package net.xenerax.sunlaser;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheSunIsADeadlyLaserClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
